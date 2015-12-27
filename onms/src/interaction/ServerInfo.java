22b61481d3387f92df3254a310282714b2505a8b
JulianLian <carcdr@outlook.com>
2015-12-27 11:19:50 +0800

    build success
    Build success

--- onms/src/interaction/ServerInfo.java
+++ onms/src/interaction/ServerInfo.java
@@ -4,18 +4,18 @@ package interaction;
  * Created by Julian on 2015/12/26.
  */
 public class ServerInfo {
-    public static String serverAddress;
-    public static final int ISUPort = 8000;
-    public static final int OTUPort = 8003;
-    public static final int FiberOpticPort = 8002;
+    static String ServerAddress = "192.168.0.5";
+    static final int ISUPort = 8000;
+    static final int OTUPort = 8003;
+    static final int FiberOpticPort = 8002;
 //    Command MODule:FUNCtions:PORT?
 
     public static void setServerAddress(String serverAddress) {
-        ServerInfo.serverAddress = serverAddress;
+        ServerInfo.ServerAddress = serverAddress;
     }
 
     public static String getServerAddress() {
-        return serverAddress;
+        return ServerAddress;
     }
 
     public static int getISUPort() {
